dd585f261369a6e6579eec0436b1184370a49867
LuckySinghRaawat <cyberboy3131@gmail.com>
2024-09-10 22:46:42 +0530

    Add files via upload

---
+++ postfixInfix.c
@@ -0,0 +1,58 @@
+//C program that converts infix expressions to postfix expressions:
+
+#include <stdio.h>
+#define MAX_SIZE 100
+
+char stack[MAX_SIZE];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int precedence(char c) {
+    if (c == '+' || c == '-')
+        return 1;
+    else if (c == '*' || c == '/')
+        return 2;
+    else if (c == '^')
+        return 3;
+    else
+        return 0;
+}
+
+void infixToPostfix(char* infix, char* postfix) {
+    int i = 0, j = 0;
+    while (infix[i] != '\0') {
+       if ((infix[i] >= 'a' && infix[i] <= 'z') || (infix[i] >= 'A' && infix[i] <= 'Z') || (infix[i] >= '0' && infix[i] <= '9'))
+            postfix[j++] = infix[i++];
+        else if (infix[i] == '(')
+            push(infix[i++]);
+        else if (infix[i] == ')') {
+            while (stack[top] != '(')
+                postfix[j++] = pop();
+            pop();
+            i++;
+        } else {
+            while (top != -1 && precedence(stack[top]) >= precedence(infix[i]))
+                postfix[j++] = pop();
+            push(infix[i++]);
+        }
+    }
+    while (top != -1)
+        postfix[j++] = pop();
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[MAX_SIZE], postfix[MAX_SIZE];
+    printf("Enter an infix expression: ");
+    fgets(infix, MAX_SIZE, stdin); // Remove newline character
+    infixToPostfix(infix, postfix);
+    printf("Postfix expression: %s\n", postfix);
+    return 0;
+}
